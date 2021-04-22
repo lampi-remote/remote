04afecf204f81a943e042303c73a92b128eae57b
Eric Han <haneric42@gmail.com>
2021-04-22 18:11:53 +0100

    Central bluetooth now turns on lamp when connected

--- bluetooth/central.js
+++ bluetooth/central.js
@@ -1,38 +1,84 @@
 var noble = require('noble');
 var allowed_devices = require('./../bt_devices.json');
 
+const OUR_SERVICE_UUID = '7a4bbfe6-999f-4717-b63a-066e06971f59';
+const LAMP_UUID = '38CA7A14-1A49-A2F1-EA69-E81D076B2457';
+const LAMP_SERVICE = '0001a7d3d8a44fea81741736e808c066';
+// const LAMP_SERVICE = '0001a7d3-d8a4-4fea-8174-1736e808c066';
+
+var LampiOnOffCharacteristic = null;
+
 // List of allowed devices
 const devices = allowed_devices.devices;
-// last advertising data received
-var lastAdvertising = {
-};
 
-function onDeviceChanged(addr, data) {
-  console.log("Device ",addr,"changed data",JSON.stringify(data));
-}
+noble.on('discover', function (peripheral) {
+  if (devices.indexOf(peripheral.address.toUpperCase()) < 0) return;
 
-function onDiscovery(peripheral) {
-  // do we know this device?
-  if (devices.indexOf(peripheral.address.toUpperCase())<0) return;
+  noble.stopScanning();
 
-  console.log("Connected!");
-//   // does it have manufacturer data with Espruino/Puck.js's UUID
-//   if (!peripheral.advertisement.manufacturerData ||
-//       peripheral.advertisement.manufacturerData[0]!=0x90 ||
-//       peripheral.advertisement.manufacturerData[1]!=0x05) return;
-//   // get just our data
-//   var data = peripheral.advertisement.manufacturerData.slice(2);
-//   // check for changed services
-//   if (lastAdvertising[peripheral.address] != data.toString())
-//     onDeviceChanged(peripheral.address, data);
-//   lastAdvertising[peripheral.address] = data;
-}
+  peripheral.connect(function (err) {
+    console.log('connected!');
 
-noble.on('stateChange',  function(state) {
-  if (state!="poweredOn") return;
+    // peripheral.discoverAllServicesAndCharacteristics(function (error, services, characteristics) {
+    //   services.forEach(function (service) {
+    //     console.log('found service:', service.uuid);
+    //   });
+
+    //   characteristics.forEach(function (characteristic) {
+    //     console.log('found characteristic:', characteristic.uuid);
+    //   });
+    // });
+
+    peripheral.discoverServices([LAMP_SERVICE], function (err, services) {
+      console.log(services)
+
+      services.forEach(function (service) {
+        console.log('found service:', service.uuid);
+
+        service.discoverCharacteristics([], function (err, characteristics) {
+          characteristics.forEach(function (characteristic) {
+            console.log('found characteristic:', characteristic.uuid);
+
+            if (characteristic.uuid === '0004a7d3d8a44fea81741736e808c066') {
+              console.log("works");
+              LampiOnOffCharacteristic = characteristic;
+              togglePower();
+            }
+          }); // characterisics
+        }); // discover characteristics
+
+      }); //services
+    }); // discover services
+  }); // connect
+});
+
+noble.on('stateChange', function (state) {
+  if (state != "poweredOn") {
+    noble.stopScanning();
+    return;
+  }
+
   console.log("Starting scan...");
   noble.startScanning([], true);
 });
-noble.on('discover', onDiscovery);
-noble.on('scanStart', function() { console.log("Scanning started."); });
-noble.on('scanStop', function() { console.log("Scanning stopped.");});
+
+noble.on('scanStart', function () {
+  console.log("Scanning started.");
+});
+
+noble.on('scanStop', function () {
+  console.log("Scanning stopped.");
+});
+
+function togglePower() {
+  var power = new Buffer(1);
+  power.writeUInt8(0x1, 0);
+
+  LampiOnOffCharacteristic.write(power, false, function(err) {
+    if (!err) {
+      console.log("works");
+    } else {
+      console.log("error");
+    }
+  })
+}
