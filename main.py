efd685095e0c13c9111d5f1153e66a2fb3b4fa62
Lucas Popp <lmp122@case.edu>
2021-04-26 00:02:41 +0100

    Update code to fix errors and improve logging

--- main.py
+++ main.py
@@ -2,21 +2,34 @@
 import RPi.GPIO as GPIO
 from input.components import RGBLED
 from input.controls import Controls
+import argparse
+import logging
 
-print("Starting...")
+# Set up the args parser and logger
+parser = argparse.ArgumentParser(description="LAMPI remote")
+parser.add_argument('--log', default='INFO', choices=['debug', 'DEBUG', 'info', 'INFO', 'warning', 'WARNING', 'error', 'ERROR', 'critical', 'CRITICAL'], required=False, dest='loglevel', type=str)
+args = parser.parse_args()
+print(args)
+
+numeric_level = getattr(logging, args.loglevel.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % loglevel)
+
+logging.basicConfig(level=numeric_level, format='%(levelname)s: %(message)s')
+
+logging.info("Starting...")
 
 controls = Controls()
 controls.setup()
 
 def power():
-    controls.led.flash_color(RGBLED.RED)
-    print("Power callback")
+    controls.led.flash_rgb(RGBLED.RED)
 
 controls.power_btn.onPress = power
 
 try:
     message = input("Press enter to quit\n\n")
 finally:
-    print("\n\nCleaning up GPIO")
+    logging.info("Cleaning up GPIO")
     controls.cleanup()
     print()
